bf964ae5da303f5e3ec110c85e66fc292df11226
akshaybawane008 <akshaybawane008gmail.com>
2025-12-06 14:58:31 +0530

    Portpolio form added

--- src/components/forms/PlacementForm.jsx
+++ src/components/forms/PlacementForm.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const PlacementForm = () => {
   return (
-    <div>
-      
+    <div className="d-flex flex-column gap-4">
+      <h1>Add Pllacement Details</h1>
+      <input type="text" placeholder="Add Name Of Student"/>
+      <input type="text" placeholder="Add Copmay Name"/>
+      <input type="file" />
     </div>
-  )
-}
+  );
+};
 
-export default PlacementForm
+export default PlacementForm;
