7cb55dda1acb485af83ade29224089ddf2ec207b
aniketxx <aniketparkhi2004@gmail.com>
2026-01-01 11:26:38 +0530

    base path changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/new/",
   plugins: [react()],
-   base: "/new/",  // 👈 VERY IMPORTANT
 })
