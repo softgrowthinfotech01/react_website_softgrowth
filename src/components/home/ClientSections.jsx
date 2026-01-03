cb7557c8eb402b6dbfbe985bf5b721ec11824c21
akshaybawane008 <akshaybawane008gmail.com>
2026-01-03 14:03:32 +0530

    Comanies images imported

--- src/components/home/ClientSections.jsx
+++ src/components/home/ClientSections.jsx
@@ -1,22 +1,38 @@
+import codewiz from "/assets/company_logos/codewiz.jpeg";
+import cognizant from "/assets/company_logos/cognizant.svg";
+import mnj from "/assets/company_logos/mnj.png";
+import plus91 from "/assets/company_logos/plus91.jpg";
+import singlepoint from "/assets/company_logos/singlepoint.webp";
+import capita from "/assets/company_logos/capita.jpeg";
+import adox from "/assets/company_logos/adox.png";
+import capgemini from "/assets/company_logos/Capgemini.svg";
+import volody from "/assets/company_logos/Volody.svg";
+import radiant from "/assets/company_logos/radiant.png";
+import kumaran from "/assets/company_logos/kumaran.png";
+import quickheal from "/assets/company_logos/Quick_Heal.svg";
+import deloitte from "/assets/company_logos/deloitte.svg";
+import hexaware from "/assets/company_logos/hexaware.svg";
+import infosys from "/assets/company_logos/infosys.svg";
+import tcs from "/assets/company_logos/tcs.svg";
 
 const ClientSections = () => {
   const companies = [
-    { src: "/src/assets/company_logos/codewiz.jpeg", delay: 0 },
-    { src: "/src/assets/company_logos/cognizant.svg", delay: 100 },
-    { src: "/src/assets/company_logos/mnj.png", delay: 200, className: "w-50" },
-    { src: "/src/assets/company_logos/plus91.jpg", delay: 300 },
-    { src: "/src/assets/company_logos/singlepoint.webp", delay: 400 },
-    { src: "/src/assets/company_logos/capita.jpeg", delay: 500, className: "w-25" },
-    { src: "/src/assets/company_logos/adox.png", delay: 500, className: "w-25" },
-    { src: "/src/assets/company_logos/Capgemini.svg", delay: 500 },
-    { src: "/src/assets/company_logos/Volody.svg", delay: 500 },
-    { src: "/src/assets/company_logos/radiant.png", delay: 500 },
-    { src: "/src/assets/company_logos/kumaran.png", delay: 500 },
-    { src: "/src/assets/company_logos/Quick_Heal.svg", delay: 500 },
-    { src: "/src/assets/company_logos/deloitte.svg", delay: 500 },
-    { src: "/src/assets/company_logos/hexaware.svg", delay: 500 },
-    { src: "/src/assets/company_logos/infosys.svg", delay: 500 },
-    { src: "/src/assets/company_logos/tcs.svg", delay: 500 },
+    { src: codewiz, delay: 0 },
+    { src: cognizant, delay: 100 },
+    { src: mnj, delay: 200, className: "w-50" },
+    { src: plus91, delay: 300 },
+    { src: singlepoint, delay: 400 },
+    { src: capita, delay: 500, className: "w-25" },
+    { src: adox, delay: 500, className: "w-25" },
+    { src: capgemini, delay: 500 },
+    { src: volody, delay: 500 },
+    { src: radiant, delay: 500 },
+    { src: kumaran, delay: 500 },
+    { src: quickheal, delay: 500 },
+    { src: deloitte, delay: 500 },
+    { src: hexaware, delay: 500 },
+    { src: infosys, delay: 500 },
+    { src: tcs, delay: 500 },
   ];
 
   return (
